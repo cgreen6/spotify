1de743852f87f765551fbc579bda642833060589
Christian Green <hoolio.suare@gmail.com>
2022-09-02 19:47:42 -0600

    corrected package.json file and fixed billboard controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_004124) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_012523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "billboards", force: :cascade do |t|
-    t.string "attr"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
